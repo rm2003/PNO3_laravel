e986842fe56ee28c7a70029dff32440f95afaecb
rm2003 <robbe.mensch@hgturnhout.be>
2022-11-30 15:02:43 +0100

    extra uitbreidingen + commentaar bijschrijven

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,53 +68,93 @@ class AuthController extends Controller
     }
     
 
+    //the function for registering a new account
     public function register(Request $request)
     {
-        
+        //This logs are to know a register request came in en wath the request was
+        //Se we can see in the log files what's happening (mainly important while the development is in proces)
         error_log("REGISTER");
         error_log($request);
-        $user= new Users;
-
         error_log(gettype($request));
-        $reqContent = json_decode($request->getContent(), true);
 
+        //Because the request comes in as a json, the json needs to be decoded to see whats inside the json
+        //also a error log to see the inside of the json
+        $reqContent = json_decode($request->getContent(), true);
+        error_log($reqContent);
+        
+        //this are rules which the request needs to fullfil
+        //to make sure all the fields were filled in
         $rules = [
-            //'UserId' => 'required',
             'email' => 'required|string',
             'licenseplate' => 'required|string',
             'password' => 'required|string'    
             ];
 
+        //the validator will checks the rules on the request
         $validator = Validator::make($reqContent, $rules);
 
+        //if the validator passes, which means all the fields were filled in, the if statement is correct
+        //els there will be sent back that not all the fields were filled in
         if ($validator->passes() ) {
-            if(Users::where('email', '=', $reqContent['email'])->exists() or Users::where('licenseplate', '=', $reqContent['licenseplate'])->exists() or strlen($reqContent['password'])<5 or strlen($reqContent['licenseplate']) != 9){
-                error_log('Een van de parameters is niet correct');
+            //check if the email or licenseplate are not yet in the database
+            //if already in the database, then sent back that it is already in the database
+            if(Users::where('email', '=', $reqContent['email'])->exists() or Users::where('licenseplate', '=', $reqContent['licenseplate'])->exists()){
+                error_log('Email or licenseplate already in the database');
+                
                 $response = [
                     'result' => "Email or licenseplate already in database",
                     'token' => "abc"
                 ];
 
                 return response($response, 405);
-                }
-            else
-            {
+
+            } //check if the licenseplate is 9 characters long, else sent back
+            elseif(strlen($reqContent['licenseplate']) != 9){
+                error_log("The licensplate is not a valid one");
+                $response = [
+                    'result' => "Licensplate is not a valid one",
+                    'token' => "abc"
+                ];
+
+                return response($response, 405);
+
+            } //check that the password is long enough (longer than 5 characters)
+            elseif(strlen($reqContent['password'])<5){
+                error_log("The password is not long enough")
+                $response = [
+                    'result' => "Password is not long enough, please choos a longer one",
+                    'token' => "abc"
+                ];
+
+                return response($response, 405);
+            }//if all the checks passes put the user in the database
+            else{
+                //create a new user in the Users database
+                $user= new Users;
+                //User id here was introduced to immidiatly delete all the test records
                 $user->UserId=1;
                 $user->email=$reqContent['email'];
                 $user->licenseplate=$reqContent['licenseplate'];
                 $password = $reqContent['password'];
+                //To store the password directly in the database is not safe
+                //That is it is hashed, but before the has is added a unique salt, with a random set digits + email
                 $password_with_salt = $password . "3Iw54#yr" . $reqContent['email'];
                 $hashed_password = password_hash($password_with_salt, PASSWORD_BCRYPT);
                 $user->password=$hashed_password;
+                //log which user is put in the database
                 error_log($user);
+                //save the users to the database
                 $user->save();
 
-                //This was how we made the token with sanctum
+                //This was how we made the token with sanctum, but not used in final product
                 //$token = $user->createToken($reqContent['email'])->plainTextToken; 
                 
+                //create a token
                 $token = $this->create_token(64);
+                //gets the actual date the token was created
                 $date = date('d-m-y H:i:s', strtotime('+ 1 hours')); //+1hour because date is in gmt, so plus 1 hour is our winter hour (time used when made)
 
+                //put the created accestoken in the database so the registered user is automatically loged in
                 $accestoken = new acces_tokens;
                 $accestoken->email = $reqContent['email'];
                 $accestoken->token = $token;
@@ -122,8 +162,7 @@ class AuthController extends Controller
                 $accestoken->created_at = $date;
                 $accestoken->save(); 
 
-                
-
+                //if everything went correctly return that is was succesful with the token
                 $response = [
                     'result' => "Registered successfully",
                     'token' => $token
@@ -131,10 +170,11 @@ class AuthController extends Controller
                 
                  return response($response, 201);
 
-        }}else{
-            error_log('Een van de parameters is niet correct');
+        }}//if not all fields are filled in
+         else{
+            error_log('Please fill in all fields');
             $response = [
-                'result' => "Something went wrong",
+                'result' => "Please fill in all fields",
                 'token' => "abc"
             ];
 
@@ -143,6 +183,7 @@ class AuthController extends Controller
     
     }
 
+    //the function for logging in a user
     public function login(Request $request)
     {
         error_log("LOGIN");
