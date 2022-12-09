0cd5c1817a8df66b8df52bf1d1d88267d73476b0
rm2003 <robbe.mensch@hgturnhout.be>
2022-12-09 16:18:11 +0100

    proberen

--- app/Http/Controllers/HistCont.php
+++ app/Http/Controllers/HistCont.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\history;
 
 class HistCont extends Controller
 {
-    //
+    public function get_history(Request $request){
+        $reqContent = json_decode($request->getContent(), true);
+
+        $rules = [
+            'email' => 'required|string',
+            'token' => 'required|string'    
+        ];
+
+        $validator = Validator::make($reqContent, $rules);
+        
+        if ($validator->fails()) {
+            $response = [
+                'result' => "Please make a valid request"
+            ];
+            error_log("Please fill in all fields ");
+            return response($response, 400);
+        }
+
+        $token = $reqContent['token'];
+        $email = $reqContent['email'];
+
+        $token_validation = app('App\Http\Controllers\request_validation')->token_validation($token, $email);
+        if($token_validation = "Request validated"){
+            list_histories_users = history::where('email', '=', $email)->get();
+        }
+
+    }
 }
